015caba0af026659a83876a855fb9b222d708c66
League Student <league@iMac-27.attlocal.net>
2019-11-03 11:28:52 -0800

    myfirstswingGUI(IP)

--- src/_02_my_first_swing_gui/MyFirstSwingGUI.java
+++ src/_02_my_first_swing_gui/MyFirstSwingGUI.java
@@ -16,20 +16,20 @@ public class MyFirstSwingGUI {
 	public void run() {
 
 		// 1. Create and initialize an object of the JFrame class
-
+JFrame yeet = new JFrame();
 		// 2. Set your JFrame object to be visible
-
+yeet.setVisible(true);
 		// 3. Run your program. Do you see your window? It's probably very
 		// small.
 
 		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+yeet.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 5. Create and initialize an object of the JPanel class
-
+JPanel yeeet = new JPanel();
 		// 6. Create and initialize an object of the JLabel class
-
+JLabel yeeeet = new JLabel();
 		// 7. Set the text of the JLabel to a lovely greeting.
-
+yeeeet.setText(Hello);
 		// 8. Add the JPanel object to the JFrame
 
 		// 9. Add the JLabel object to the JPanel
