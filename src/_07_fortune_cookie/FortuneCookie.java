2c13bead73051930e8fb5588925d3ed0a8166327
League Student <league@iMac-16.attlocal.net>
2020-02-02 11:20:18 -0800

    FortuneCookie,JackInTheBox,SoundEffectsMachine(IP)

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -2,6 +2,7 @@ package _07_fortune_cookie;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -26,6 +27,24 @@ public class FortuneCookie implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		int rand = new Random().nextInt(5);
+	System.out.println(rand);
+	if(rand == 0) {
+	System.out.println("Some one in your life will come to comfort you at the worst time");
+	}
+	if(rand == 1) {
+	System.out.println("A beautiful, smart, and loving person will be coming into your life");	
+	}
+	if(rand == 2) {
+	System.out.println("Advice is like kissing. It costs nothing and is a pleasant thing to do");	
+	}
+	if(rand == 3) {
+	System.out.println("An agreeable romance might begin to take on the appearance");
+	}
+	if(rand == 4) {
+	System.out.println("It is better to be an optimist and proven a fool than to be a pessimist and be proven right");
+	}
+	
+	
 	}
 }
