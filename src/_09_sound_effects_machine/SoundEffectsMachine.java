5dda3c19d619b161032681aca3295624cd9f40da
League Student <league@iMac-16.attlocal.net>
2020-02-16 11:21:51 -0800

    SoundEffectsMachine(ip)

--- src/_09_sound_effects_machine/SoundEffectsMachine.java
+++ src/_09_sound_effects_machine/SoundEffectsMachine.java
@@ -1,5 +1,30 @@
 package _09_sound_effects_machine;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class SoundEffectsMachine {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+public void showButton() {
+  frame.setVisible(true);
+frame.add(panel);
+JButton button = new JButton();
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
 
 }
--- src/_09_sound_effects_machine/SoundEffectsMachineRunner.java
+++ src/_09_sound_effects_machine/SoundEffectsMachineRunner.java
@@ -1,5 +1,8 @@
 package _09_sound_effects_machine;
 
 public class SoundEffectsMachineRunner {
-
+	public static void main(String[] args) {
+		SoundEffectsMachine machine = new SoundEffectsMachine();
+		machine.showButton();
+	}
 }
