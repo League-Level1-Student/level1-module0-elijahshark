5477ad222b37f9075bd8ab576778dfac1e3f8593
League Student <league@iMac-16.attlocal.net>
2020-02-23 11:28:20 -0800

    SoundEffectsMachine(IP)

--- src/_09_sound_effects_machine/SoundEffectsMachine.java
+++ src/_09_sound_effects_machine/SoundEffectsMachine.java
@@ -1,16 +1,34 @@
 package _09_sound_effects_machine;
 
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class SoundEffectsMachine {
+public class SoundEffectsMachine implements ActionListener {
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
+JButton button = new JButton();
+JButton button1 = new JButton();
+JButton button2 = new JButton();
+JButton button3 = new JButton();
 public void showButton() {
   frame.setVisible(true);
 frame.add(panel);
-JButton button = new JButton();
+panel.add(button);
+panel.add(button1);
+panel.add(button2);
+panel.add(button3);
+frame.pack();
+button.addActionListener(this);
+button1.addActionListener(this);
+button2.addActionListener(this);
+button3.addActionListener(this);
+
 
 
 
@@ -21,10 +39,25 @@ JButton button = new JButton();
 
 
 }
+private void playSound(String fileName) {
+    AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+    sound.play();
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+if(button == e.getSource())	{
+playSound();
+}
+if(button1 == e.getSource())	{
 
+}
+if(button2 == e.getSource())	{
 
+}
+if(button3 == e.getSource())	{
 
+}
 
-
-
+}
 }
