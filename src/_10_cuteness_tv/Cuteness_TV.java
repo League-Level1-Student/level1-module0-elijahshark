200d957bf7952778ad90fbbeda07a9a57080a53d
root <root@c3ac9ee13b4f>
2020-04-05 18:27:29 +0000

    LIFE

---
+++ src/_10_cuteness_tv/Cuteness_TV.java
@@ -0,0 +1,19 @@
+package _10_cuteness_tv;
+
+public class Cuteness_TV {
+public void showbutton() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ src/_10_cuteness_tv/Cuteness_TV_Runner.java
@@ -0,0 +1,8 @@
+package _10_cuteness_tv;
+
+public class Cuteness_TV_Runner {
+public static void main(String[] args) {
+	Cuteness_TV machine = new Cuteness_TV();
+machine.showbutton();
+}
+}
