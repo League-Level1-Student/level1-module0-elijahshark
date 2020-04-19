5a62c8d0f90b556631a2d90399a155f5cfe07072
root <root@2afa7ba4ef1c>
2020-04-19 17:53:53 +0000

    Yeet

--- src/_10_cuteness_tv/Cuteness_TV.java
+++ src/_10_cuteness_tv/Cuteness_TV.java
@@ -1,7 +1,36 @@
 package _10_cuteness_tv;
 
-public class Cuteness_TV {
-public void showbutton() {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Cuteness_TV implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton Ducks = new JButton();
+	JButton Frog = new JButton();
+	JButton FluffyUnicorns = new JButton();
+	
+	
+	public void showbutton() {
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(Ducks);
+		panel.add(Frog);
+		panel.add(FluffyUnicorns);
+		frame.pack();
+		Ducks.addActionListener(this);
+		Frog.addActionListener(this);
+		FluffyUnicorns.addActionListener(this);
+
+
+
+
+		
 
 
 
@@ -9,11 +38,38 @@ public void showbutton() {
 
 
 
+}
+void showDucks() {
+    playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+}
 
+void showFrog() {
+    playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+}
 
+void showFluffyUnicorns() {
+    playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+}
 
-
-
-
+void playVideo(String videoID) {
+    try {
+         URI uri = new URI(videoID);
+         java.awt.Desktop.getDesktop().browse(uri);
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if(FluffyUnicorns == e.getSource())	{
+		System.out.println("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+		}
+		if(Frog == e.getSource())	{
+			System.out.println("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+		}
+		if(Ducks == e.getSource())	{
+			System.out.println("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+		}
 }
 }
